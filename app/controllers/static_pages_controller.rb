49c554d3baff35470b5ff12bf9ba73c560cbb86d
Leandro Pereira Gomes <grupolpgomes@gmail.com>
2016-04-01 16:21:54 -0300

    incluindo migrete tabela produtos

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,6 +1,10 @@
 class StaticPagesController < ApplicationController
  
  def home
+
+ 
+ Produto.copy_from "medicamento.csv"
+ 
   if user_signed_in?
     if !current_user.dadosuser
 	 	@dadosuser = Dadosuser.new
