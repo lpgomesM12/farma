08b2092bf972f96c84cca8d03cd7d10e0b994fb9
Leandro Pereira Gomes <grupolpgomes@gmail.com>
2016-03-31 23:01:47 -0300

    retirando migrete

--- db/migrate/20160322145924_add_sitorcamento_ref_to_orcamentorealizado.rb
+++
@@ -1,5 +0,0 @@
-class AddSitorcamentoRefToOrcamentorealizado < ActiveRecord::Migration
-  def change
-    add_reference :orcamentorealizados, :situacaoorcamento, index: true, foreign_key: true
-  end
-end
